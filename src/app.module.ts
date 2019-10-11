b1279c5fb81023c85a797dde9a749c196e8be88e
Panupong <5635512110@psu.ac.th>
2019-10-12 06:10:50 +0700

    learn module and middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { CatsController } from './cats/cats.controller';
 import { HelpersModule } from './helpers/helpers.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 @Module({
   imports: [CatsModule, HelpersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule  {
+}
--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
-
+import { LoggerMiddleware } from '../common/middleware/logger.middleware';
 @Module({
     controllers: [CatsController],
     providers: [CatsService],
     exports: [CatsService]
 })
-export class CatsModule {}
+export class CatsModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply(LoggerMiddleware)
+            .forRoutes(CatsController);
+    }
+}
