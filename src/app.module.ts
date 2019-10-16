33f8b7b830958e0c532f1804a808468300afe70e
Panupong <5635512110@psu.ac.th>
2019-10-17 06:30:03 +0700

    learn pipes and exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module, NestModule, MiddlewareConsumer, ValidationPipe } from '@nestjs/common';
-import { APP_PIPE } from '@nestjs/core';
+import { APP_PIPE, APP_FILTER } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { CatsController } from './cats/cats.controller';
 import { HelpersModule } from './helpers/helpers.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { AllExceptionsFilter } from './common/exceptionFilters/http-exception.filter';
 // import { ValidationPipe } from './common/pipes/validation.pipe';
 @Module({
   imports: [CatsModule, HelpersModule],
@@ -15,6 +16,10 @@ import { LoggerMiddleware } from './common/middleware/logger.middleware';
     {
       provide: APP_PIPE,
       useClass: ValidationPipe
+    },
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter
     }
   ],
 })
